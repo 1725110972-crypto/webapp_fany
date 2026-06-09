30667267aab9202278341ccd1060556f390fc6ad
Stephanie Santander <1725110972@utectulancingo.edu.mx>
2026-06-09 05:07:42 +0000

    CREATED valores usando llaves y los convierto a números al mismo tiempo

--- demos/calculadora/app.py
+++ demos/calculadora/app.py
@@ -18,7 +18,14 @@ class Calculadora:
     
     def POST(self):
         formulario = web.input()
-        return f"Formulario: {formulario}"
+        #valores usando sus llaves y los conviertes a números al mismo tiempo
+        numero_uno = float(formulario.numero_1)
+        numero_dos = float(formulario.numero_2)
+        
+        resultado = numero_uno + numero_dos
+        
+        # 4. Retornamos el texto original que ya tenías junto con el resultado de la suma
+        return f"Formulario: {formulario} | resultado: {resultado}"
 
 if __name__ == "__main__":
     app.run()
